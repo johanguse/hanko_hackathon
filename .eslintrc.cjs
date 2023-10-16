927db8fac543341a4526a86e3d4c5e5869c29598
Johan Guse <johanguse@gmail.com>
2023-10-16 15:44:27 -0300

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,9 +20,9 @@ const config = {
       },
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
   },
   settings: {
     tailwindcss: {
