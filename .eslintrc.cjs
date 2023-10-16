760a9954dd29f1b0b74ea04b3f5e982826218c0b
Johan Guse <johanguse@gmail.com>
2023-10-16 15:42:01 -0300

    add no-unsafe-member-access

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ const config = {
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
   },
   settings: {
     tailwindcss: {
