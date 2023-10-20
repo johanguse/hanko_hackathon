2bc0f2cae4116445e92ee952a890400c0e8460a4
Johan Guse <johanguse@gmail.com>
2023-10-19 23:10:58 -0300

    fix: ts error on vercel deployment

--- src/app/[locale]/(dashboard)/dashboard/settings/page.tsx
+++ src/app/[locale]/(dashboard)/dashboard/settings/page.tsx
@@ -1,9 +1,6 @@
-import { getSiteConfig } from '@/config/siteConfig'
 import { Text } from '@/components/common'
 
 export default async function Page() {
-  const { mainNav, name } = await getSiteConfig()
-
   return (
     <>
       <Text labelToken="Settings" medium />
--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -1,4 +1,5 @@
-// @ts-nocheck
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 
 import { complement, either, equals, isNil } from 'ramda'
 
