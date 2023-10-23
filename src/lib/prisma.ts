36c2e7f65b59162c914563cd1c3568b37217052d
Johan Guse <johanguse@gmail.com>
2023-10-23 19:41:21 -0300

    Fix: ts error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,14 @@
-/* eslint-disable no-use-before-define */
-
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined //eslint-disable-line no-var
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
 }
-
-const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
