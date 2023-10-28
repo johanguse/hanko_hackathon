3bac2954f98e88cc4ff735146d08b058da942a56
Johan Guse <johanguse@gmail.com>
2023-10-28 16:12:29 -0300

    fix: supabase client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,37 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const {
-  NEXT_PUBLIC_SUPABASE_PROJECT_URL,
-  NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
-  NEXT_PUBLIC_SUPABASE_BUCKET,
-} = process.env
-
-function validateEnvVariable(variable: string | undefined, name: string) {
-  if (!variable) {
-    throw new Error(`Missing ${name} environment variable file`)
-  }
-}
-
-validateEnvVariable(
-  NEXT_PUBLIC_SUPABASE_PROJECT_URL,
-  'NEXT_PUBLIC_SUPABASE_PROJECT_URL'
-)
-validateEnvVariable(
-  NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
-  'NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY'
-)
-
 const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
-  NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 )
 
-let BUCKET_NAME: string
-
-if (NEXT_PUBLIC_SUPABASE_BUCKET) {
-  BUCKET_NAME = NEXT_PUBLIC_SUPABASE_BUCKET
-} else {
-  throw new Error('Missing NEXT_PUBLIC_SUPABASE_BUCKET environment variable')
-}
+const BUCKET_NAME = process.env.NEXT_PUBLIC_SUPABASE_BUCKET!
 
 export { supabase, BUCKET_NAME }
