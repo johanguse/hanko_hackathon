4e3278013561460aaf84d37e35fa45be6bf67ec4
Johan Guse <johanguse@gmail.com>
2023-10-30 11:37:35 -0300

    feat: improve visual login / logout button

--- src/components/auth/ButtonLogout.tsx
+++ src/components/auth/ButtonLogout.tsx
@@ -53,12 +53,16 @@ export function LogoutBtn() {
         <div className="mx-auto flex w-fit flex-col justify-center pt-10 text-center lg:flex-row lg:pt-0">
           <Button
             variant="tertiary"
-            className="mb-4 mr-0 block lg:mb-0 lg:mr-4"
+            className="mb-4 mr-0 block justify-center lg:mb-0 lg:mr-4"
           >
-            <Link href="/login">Login</Link>
+            <Link href="/login" className="px-8 py-4">
+              Login
+            </Link>
           </Button>
-          <Button variant="primary" className="text-center">
-            <Link href="/register">Sign-up</Link>
+          <Button variant="primary" className="justify-center text-center">
+            <Link href="/register" className="px-8 py-4">
+              Sign-up
+            </Link>
           </Button>
         </div>
       )}
